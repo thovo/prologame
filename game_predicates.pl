927f2e5fb552586eea91974a0ffe6fea41aba7c9
ahmedadelhassan <ahmed.adel.hassan@hotmail.com>
2015-01-13 17:49:20 +0100

    Game loop predicate working
    Fixed the game loop predicate, using recursion, and added new utility predicates. Also added preliminary version of the naive sum heuristic

--- game_predicates.pl
+++ game_predicates.pl
@@ -9,15 +9,15 @@ play369(Size, Heuristic):-
 	Computer_score = 0,
 	Player_score = 0,
 	Turns = 0,
-	Board_matrix = [[0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]],
-	
 	% Main Game Loop
-	repeat,
 	
+	create_board(Size, Board_matrix),
 	draw_board(Board_matrix),
+	play(Size, Heuristic, Board_matrix, Computer_score, Player_score, Turns).
 	
+play(Size, Heuristic, Board_matrix, Computer_score, Player_score, Turns):-
 	select_move(Board_matrix, AI_move, Heuristic),
-	check_valid_move(Board_matrix, AI_move),
+	% check_valid_move(Board_matrix, AI_move),
 	
 	write('Computer: Here is my move: '),
 	write(AI_move),
@@ -26,47 +26,53 @@ play369(Size, Heuristic):-
 	draw_board(New_board),
 	% Board_matrix is New_board,
 	
-	update_score(New_board, AI_move, New_AI_score),
-	Total_Computer_score is Computer_score + New_AI_score,
+	% update_score(New_board, AI_move, New_AI_score),
+	% Total_Computer_score is Computer_score + New_AI_score,
 	
 	show_score(Player_score, Total_Computer_score),
+	Next_turn is Turns + 1,
 	
 	% User input part
 	% ......some code....... %
+	write('Make your move: '),
 	read(User_move),
 	
 	check_valid_move(New_board, User_move),
 	
-	update_board(New_board, User_move, P_New_board),
+	update_board(New_board, User_move, New_board2),
+	draw_board(New_board2),
 	% Board_matrix is New_board,
 	
-	update_score(P_New_board, User_move, New_Player_score),
-	Total_Player_score is Player_score + New_Player_score,
+	% update_score(P_New_board, User_move, New_Player_score),
+	% Total_Player_score is Player_score + New_Player_score,
 	
 	show_score(Total_Player_score, Total_Computer_score),
 	
 	% Check gameover condition
-	Next_turn is Turns + 1,
-	gameover(Size, Next_turn).
+	Next_turn2 is Next_turn + 1,
+	gameover(Size, Next_turn2),
+	play(Size, Heuristic, New_board2, Computer_score, Human_score, Next_turn).
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Gameover predicate:
 gameover(Size, Turns):-
-	Turns < Size*Size,
-	fail.	
+	Turns < Size*Size.
+	% fail.	
 gameover(Size, Turns):-
 	Turns =:= Size*Size,
 	write('Game Over!'),
-	show_score(Human_score, Computer_score).
+	show_score(Human_score, Computer_score),
+	fail.
 	
 	
 % Select AI move predicate
 select_move(Board_matrix, AI_move, Heuristic):-
 	Heuristic =:= 1,
-	% naive_sum(Board_matrix, Weight_matrix),
-	% get_mat_max(Weight_matrix, AI_move).
-	AI_move = [1,1].
+	naive_sum(Board_matrix, Weight_matrix),
+	get_mat_max(Weight_matrix, Max),
+	max_index(Weight_matrix, Max, Row, Col),
+	AI_move = [Row,Col].
 select_move(Board_matrix, AI_move, Heuristic):-
 	Heuristic =:= 2,
 	leveled_sum(Board_matrix, Weight_matrix),
@@ -163,6 +169,10 @@ get_mat_max([H|Matrix], Max_H):-
 	max_list(H, Max_H),
 	Max_H >= Max.
 	
+max_index(Matrix, Value, Row, Col):-
+  nth0(Row, Matrix, MatrixRow),
+  nth0(Col, MatrixRow, Value).
+	
 % Stub draw_board predicate
 draw_board([]):-
 	write('\n-------------\n').
@@ -170,3 +180,46 @@ draw_board([H|Board_matrix]):-
 	write('\n-------------\n'),
 	write(H),
 	draw_board(Board_matrix).
+	
+get_row([X,_], Board_matrix, Row):-
+	X_fix is X - 1,
+	nth0(X_fix, Board_matrix, Row).
+	
+get_col(_, [], []):- !.
+get_col([_,Y], [H|Board_matrix], [HC|Col]):-
+	Y_fix is Y - 1,
+	nth0(Y_fix, H, HC),
+	get_col([_,Y], Board_matrix, Col).
+	
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Calculating the naive sum heuristic
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+naive_sum(Board_matrix, Weight_matrix):-
+	naive_sum([1,1], Board_matrix, Weight_matrix).
+
+naive_sum([X,Y], Board_matrix, [HW|Weight_matrix]):-
+	length(Board_matrix, Size),
+	X =< Size,
+	naive_sum_row([X,Y], Board_matrix, HW),
+	Xn is X + 1,
+	naive_sum([Xn,Y], Board_matrix, Weight_matrix).
+
+naive_sum([X,Y], Board_matrix, []):-
+	length(Board_matrix, Size),
+	X > Size.
+
+naive_sum_row([X,Y], Board_matrix, [H|Row_weights]):-
+	length(Board_matrix, Size),
+	Y =< Size,
+	get_row([X,_], Board_matrix, Row),
+	get_col([_,Y], Board_matrix, Col),
+	sum_list(Row, Row_sum),
+	sum_list(Col, Col_sum),
+	max_list([Row_sum, Col_sum], H),
+	Yn is Y + 1,
+	naive_sum_row([X, Yn], Board_matrix, Row_weights).
+
+naive_sum_row([X,Y], Board_matrix, []):-
+	length(Board_matrix, Size),
+	Y > Size.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
